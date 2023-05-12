acad76c838afbf1d6701c562bebee2fd248a8ef0
Rui Pedro Gomes <51448746+Isomeg@users.noreply.github.com>
2023-05-12 22:44:54 +0100

    First Version v1.0

---
+++ qr_gen.py
@@ -0,0 +1,27 @@
+import qrcode
+import os
+
+
+dir_path = './QRCODES/'
+count = 01
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=7,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=30,
+        border=1,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save('./QRCODES/qr' + str(count).zfill(5) + '.jpg', 'JPEG')
+
+for path in os.listdir(dir_path):
+
+    if os.path.isfile(os.path.join(dir_path, path)):
+        count += 1
+
+url = input("Enter your url: ")
+generate_qrcode(url)
